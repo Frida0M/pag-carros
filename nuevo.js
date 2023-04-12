ffbb63dcd27d8954c9a0853c2efed31baa22bebb
Sack Valdez <rauldaxo@gmail.com>
2023-04-11 20:34:26 -0600

    Cambios en la rama main
    Holap

---
+++ nuevo.js
@@ -0,0 +1,35 @@
+var imagen=document.querySelector(".slide-img");
+      boton1=document.querySelector(".boton1");
+      boton2=document.querySelector(".boton2");
+      contador=0;
+      arreglo=["./img/carro1.jpg","./img/carro2.jpg","./img/carro3.jpg"];
+      boton2.style.display="none";
+
+      boton1.addEventListener("click", function(){
+        imagen.setAttribute("src",arreglo[contador]);
+        contador++;
+        if(contador>=3){
+          boton2.style.display="block";
+          boton1.style.display="none";
+        }else if (contador!=3){
+          boton2.style.display="block";
+        }
+        console.log(arreglo[contador]);
+        console.log(contador);
+      });
+
+
+      boton2.addEventListener("click", function(){
+        imagen.setAttribute("src",arreglo[contador]);
+        contador--;
+        if (contador<=0){
+          boton2.style.display="none";
+          boton1.style.display="block";
+        }else if (contador!=0){
+          boton1.style.display="block";
+        }
+        
+        
+        console.log(arreglo[contador]);
+        console.log(contador);
+      });
