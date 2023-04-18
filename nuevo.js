9546bb5321698fba9e75cc56067583d6803e5e3b
Frida0M <folivera10@gmail.com>
2023-04-18 17:03:42 -0600

    mejoras tanto de js como css

--- nuevo.js
+++ nuevo.js
@@ -1,35 +1,75 @@
-/* var imagen=document.querySelector(".slide-img");
-      boton1=document.querySelector(".boton1");
-      boton2=document.querySelector(".boton2");
-      contador=0;
-      arreglo=["./img/carro1.jpg","./img/carro2.jpg","./img/carro3.jpg"];
-      boton2.style.display="none";
+var carrito = [];
+var seHaAgregadoBotonPago = false;
+var total = 0;
 
-      boton1.addEventListener("click", function(){
-        imagen.setAttribute("src",arreglo[contador]);
-        contador++;
-        if(contador>=3){
-          boton2.style.display="block";
-          boton1.style.display="none";
-        }else if (contador!=3){
-          boton2.style.display="block";
-        }
-        console.log(arreglo[contador]);
-        console.log(contador);
-      });
+function agregarAlCarrito(elemento) {
+  var titulo = elemento.getAttribute("titulo-item");
+  var precio = parseInt(elemento.getAttribute("precio-item"));
+
+  carrito.push({ titulo: titulo, precio: precio });
+
+  actualizarCarrito();
+  actualizarTotal();
+}
+
+function eliminarDelCarrito(elemento) {
+  var indice = elemento.getAttribute("indice-item");
+  carrito.splice(indice, 1);
+  actualizarCarrito();
+  actualizarTotal();
+}
+
+function actualizarCarrito() {
+  var carritoDOM = document.querySelector("#carrito ul");
+  carritoDOM.innerHTML = "";
+
+  carrito.forEach(function (item, indice) {
+    var li = document.createElement("li");
+    li.textContent = item.titulo + " - $" + item.precio;
+    
+    var eliminarBtn = document.createElement("button");
+    eliminarBtn.textContent = "Eliminar";
+    eliminarBtn.setAttribute("indice-item", indice);
+    eliminarBtn.addEventListener("click", function() {
+      eliminarDelCarrito(this);
+    });
+    
+    li.appendChild(eliminarBtn);
+    carritoDOM.appendChild(li);
+  });
+}
 
 
-      boton2.addEventListener("click", function(){
-        imagen.setAttribute("src",arreglo[contador]);
-        contador--;
-        if (contador<=0){
-          boton2.style.display="none";
-          boton1.style.display="block";
-        }else if (contador!=0){
-          boton1.style.display="block";
-        }
-        
-        
-        console.log(arreglo[contador]);
-        console.log(contador);
-      }); */
+function actualizarTotal() {
+  total = 0;
+
+  carrito.forEach(function (item) {
+    total += item.precio;
+  });
+
+  var totalDOM = document.querySelector("#total");
+  totalDOM.textContent = total;
+
+  if (!seHaAgregadoBotonPago) {
+    var pagarBtn = document.createElement("button");
+    pagarBtn.textContent = "Pagar";
+    pagarBtn.addEventListener("click", function() {
+      alert("Uno de nuestros asesores se contactará en breve para la cita");
+      carrito = [];
+      actualizarCarrito();
+      actualizarTotal();
+      seHaAgregadoBotonPago = false;
+      var botonPagoContenedor = document.querySelector("#boton-pago-contenedor");
+      botonPagoContenedor.parentNode.removeChild(botonPagoContenedor);
+    });
+
+    var botonPagoContenedor = document.createElement("div");
+    botonPagoContenedor.setAttribute("id", "boton-pago-contenedor");
+    botonPagoContenedor.appendChild(pagarBtn);
+
+    var carritoDOM = document.querySelector("#carrito");
+    carritoDOM.parentNode.appendChild(botonPagoContenedor);
+
+    seHaAgregadoBotonPago = true;
+  }
+}
